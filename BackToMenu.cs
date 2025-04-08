7aa2acef123c48b4640bae169525423a5852c822
Duy <Duyhoang1147@gmail.com>
2025-04-08 09:36:23 +0700

    SCRUM-75: menu update

--- BackToMenu.cs
+++ BackToMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//class for back to menu button
+//back to menu button
 public class BackToMenu : MonoBehaviour
 {
     //button trigger
